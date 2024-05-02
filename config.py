9fa282ab809eac1a55a4301852f1cd781482225b
˹ ɪ'ϻ ᴛ ᴏ x ɪ ᴄ ˼ ×͜™ <143880963+SH251204@users.noreply.github.com>
2024-05-02 15:16:55 +0000

    Update config.py
    Signed-off-by: ˹ ɪ'ϻ ᴛ ᴏ x ɪ ᴄ ˼ ×͜™  <143880963+SH251204@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SH251204/OnlyMusixBUG",
+    "https://github.com/SH251204/BuGMusix",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
