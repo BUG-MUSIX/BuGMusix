b266819c9d90a1cb214658a98eb127b1d8a47f3c
𝐒 ᴏ ʜ ᴇ ʟ ..!! <143880963+SH251204@users.noreply.github.com>
2024-05-10 14:19:12 +0000

    Update bot_add.py
    Signed-off-by: 𝐒 ᴏ ʜ ᴇ ʟ ..!! <143880963+SH251204@users.noreply.github.com>

--- AnonXMusic/plugins/tools/bot_add.py
+++ AnonXMusic/plugins/tools/bot_add.py
@@ -37,12 +37,12 @@ async def join_watcher(_, message):
                     message.chat.username if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐆ʀᴏᴜᴘ"
                 )
                 msg = (
-                    f"**📝𝐌ᴜsɪᴄ 𝐁ᴏᴛ 𝐀ᴅᴅᴇᴅ 𝐈ɴ 𝐀 #𝐍ᴇᴡ_𝐆ʀᴏᴜᴘ**\n\n"
-                    f"**📌𝐂ʜᴀᴛ 𝐍ᴀᴍᴇ:** {message.chat.title}\n"
-                    f"**🍂𝐂ʜᴀᴛ 𝐈ᴅ:** {message.chat.id}\n"
-                    f"**🔐𝐂ʜᴀᴛ 𝐔sᴇʀɴᴀᴍᴇ:** @{username}\n"
-                    f"**📈𝐆ʀᴏᴜᴘ 𝐌ᴇᴍʙᴇʀs:** {count}\n"
-                    f"**🤔𝐀ᴅᴅᴇᴅ 𝐁ʏ:** {message.from_user.mention}"
+                    f"📝𝐌ᴜsɪᴄ 𝐁ᴏᴛ 𝐀ᴅᴅᴇᴅ 𝐈ɴ 𝐀 #𝐍ᴇᴡ_𝐆ʀᴏᴜᴘ\n\n"
+                    f"📌𝐂ʜᴀᴛ 𝐍ᴀᴍᴇ: {message.chat.title}\n"
+                    f"🍂𝐂ʜᴀᴛ 𝐈ᴅ: {message.chat.id}\n"
+                    f"🔐𝐂ʜᴀᴛ 𝐔sᴇʀɴᴀᴍᴇ: @{username}\n"
+                    f"📈𝐆ʀᴏᴜᴘ 𝐌ᴇᴍʙᴇʀs: {count}\n"
+                    f"🤔𝐀ᴅᴅᴇᴅ 𝐁ʏ: {message.from_user.mention}"
                 )
                 await app.send_photo(
                     LOG_GROUP_ID,
